c3a1a50269d92898b1ec11d29ff8c81be1aadec7
Dinesh Namburi <n.dinesh36@gmail.com>
2017-02-01 12:37:06 -0500

    Added Code to pull data from tumblr api

--- TumblrFeed/PhotosViewController.swift
+++ TumblrFeed/PhotosViewController.swift
@@ -8,12 +8,46 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
-
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var posts : [NSDictionary] = []
+    @IBOutlet weak var picsTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        picsTableView.dataSource = self
+        picsTableView.delegate = self
         // Do any additional setup after loading the view.
+        
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        
+                        // This is where you will store the returned array of posts in your posts property
+                        // self.feeds = responseFieldDictionary["posts"] as! [NSDictionary]
+                        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+                        //print("responseDictionary: \(self.posts)")
+                        self.picsTableView.reloadData()
+                    }
+                }
+        });
+        task.resume()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +55,15 @@ class PhotosViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        print ("\(self.posts.count)")
+            return self.posts.count
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = picsTableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath)
+        cell.textLabel?.text = "\(self.posts[indexPath.row]["short_url"])"
+        return cell
+    }
     /*
     // MARK: - Navigation
 
